e41b54ac32f1ce587d4984554e2a33ad02a7e1be
Jiri Chmelik <chmelikj@vut.cz>
2024-04-08 12:33:40 +0200

    naive search

--- searching.py
+++ searching.py
@@ -15,12 +15,25 @@ def read_data(file_name, field):
     file_path = os.path.join(cwd_path, file_name)
     with open(file_path) as data_file:
         data = json.load(data_file)
-    return data[field]
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
+
+
+def linear_search(sequence, searched_number):
+    list_of_positions = []
+    for position, number in enumerate(sequence):
+        if number == searched_number:
+            list_of_positions.append(position)
+    return {'positions': list_of_positions, 'count': len(list_of_positions)}
 
 
 def main():
     sequential_data = read_data('sequential.json', 'unordered_numbers')
     print(sequential_data)
+    search_number_info = linear_search(sequential_data, 0)
+    print(search_number_info)
 
 
 if __name__ == '__main__':
