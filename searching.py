383f10786d2a7b77e07300d82ecf98d49e681a10
Jiri Chmelik <chmelikj@vut.cz>
2024-04-08 13:03:32 +0200

    dna naive search

--- searching.py
+++ searching.py
@@ -29,11 +29,29 @@ def linear_search(sequence, searched_number):
     return {'positions': list_of_positions, 'count': len(list_of_positions)}
 
 
+def pattern_search(sequence, searched_pattern):
+    pattern_length = len(searched_pattern)
+    list_of_positions = []
+    for position in range(len(sequence)-pattern_length):
+        is_same = True
+        for subind in range(len(searched_pattern)):
+            if sequence[position+subind] == searched_pattern[subind]:
+                is_same = is_same and True
+            else:
+                is_same = is_same and False
+        if is_same:
+            list_of_positions.append(position+pattern_length // 2)
+    return {'positions': list_of_positions, 'count': len(list_of_positions)}
+
+
 def main():
     sequential_data = read_data('sequential.json', 'unordered_numbers')
     print(sequential_data)
     search_number_info = linear_search(sequential_data, 0)
     print(search_number_info)
+    dna_data = read_data('sequential.json', 'dna_sequence')
+    search_pattern_info = pattern_search(dna_data, 'GAC')
+    print(search_pattern_info)
 
 
 if __name__ == '__main__':
