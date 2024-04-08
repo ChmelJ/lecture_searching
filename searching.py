b9b36a017b1a0b87c1bd77fda40e093103ba6412
Jiri Chmelik <chmelikj@vut.cz>
2024-04-08 12:11:11 +0200

    reading sequence

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,10 +13,14 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path) as data_file:
+        data = json.load(data_file)
+    return data[field]
 
 
 def main():
-    pass
+    sequential_data = read_data('sequential.json', 'unordered_numbers')
+    print(sequential_data)
 
 
 if __name__ == '__main__':
